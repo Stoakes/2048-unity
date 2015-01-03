67c2b01cace205f68a1b54b6b65eeb3b87cc68d4
Stoakes <_stoakes on twitter>
2015-01-03 09:32:25 +0100

    General Improvement
    Added touch devices support
    UI improvement
    Responsive design (button are positionned relatively to the screen
    width)
    Game extended from  2048 to 65365

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -11,10 +11,12 @@ public class GridManager : MonoBehaviour {
   private static float horizontalSpacingOffset = -1.65f;
   private static float verticalSpacingOffset = 1.65f;
   private static float borderSpacing = 0.1f;
-  private static float resetButtonWidth = 80f;
-  private static float resetButtonHeight = 40f;
-  private static float gameOverButtonWidth = 150f;
-  private static float gameOverButtonHeight = 300f;
+  private static float resetButtonWidth = 200f;
+  private static float resetButtonHeight = 80f;
+  private static float gameOverButtonWidth = 300f;
+  private static float gameOverButtonHeight = 300f;  
+  private static float quitButtonWidth = 200f;
+  private static float quitButtonHeight = 80f;
   private static float halfTileWidth = 0.55f;
   private static float spaceBetweenTiles = 1.1f;
 
@@ -23,48 +25,64 @@ public class GridManager : MonoBehaviour {
   private GUIText scoreText;
   private Rect resetButton;
   private Rect gameOverButton;
+  private Rect quitButton;
 
   public GameObject noTile;
   public GameObject scoreObject;
   public GameObject[] tilePrefabs;
   public LayerMask backgroundLayer;
   public Transform resetButtonTransform;
-
+  public Texture ResetTexture; // texture of the reset button
+  public Texture GameOverTexture; // texture of the game over button
+  public Texture QuitTexture; // texture of the quit button
   private enum State {
-    Loaded, 
-    WaitingForInput, 
-    CheckingMatches,
-    GameOver
+    Loaded,WaitingForInput,CheckingMatches,GameOver
   }
-
   private State state;
+  
+  // touch management 
+	public float minSwipeDistY;
+	public float minSwipeDistX;
+	private Vector2 startPos;
+	private enum TouchMovement{
+		Up, Down, Right,Left,None
+		}
+	private TouchMovement touchMove;
+
 
   #region monodevelop
   void Awake() {
     tiles = new List<GameObject>();
     state = State.Loaded;
     scoreText = scoreObject.GetComponent<GUIText>();
-    Vector3 resetButtonWorldPosition = Camera.main.WorldToScreenPoint(new Vector3(resetButtonTransform.position.x,
-                                                                                  -resetButtonTransform.position.y,
-                                                                                  resetButtonTransform.position.z));
-    resetButton = new Rect(resetButtonWorldPosition.x,
-                           resetButtonWorldPosition.y,
+
+    resetButton = new Rect(Screen.width*0.75f,
+                           Screen.width*0.15f,
                            resetButtonWidth,
                            resetButtonHeight);
+	
+	quitButton = new Rect(Screen.width*0.75f,
+                           Screen.width*0.25f,
+                           quitButtonWidth,
+                           quitButtonHeight);
     
-    Vector3 gameOverButtonWorldPosition = Camera.main.WorldToScreenPoint(new Vector3(-1f, -1f, 0f));
-    gameOverButton = new Rect(gameOverButtonWorldPosition.x,
-                              gameOverButtonWorldPosition.y,
+    gameOverButton = new Rect(Screen.width*0.15f,
+                              Screen.height*0.15f,
                               gameOverButtonWidth,
                               gameOverButtonHeight);
   }
 
   void OnGUI() {
-    if (GUI.Button(resetButton, "Reset")) {
+   GUI.backgroundColor = Color.clear;
+
+    if (GUI.Button(resetButton, ResetTexture)) {
       Reset();
     }
+	    if (GUI.Button(quitButton, QuitTexture)) {
+        Application.Quit();
+    }
     if (state == State.GameOver) {
-      if (GUI.Button(gameOverButton, "Game Over")) {
+      if (GUI.Button(gameOverButton, GameOverTexture)) {
         Reset();
       }
     }
@@ -75,20 +93,51 @@ public class GridManager : MonoBehaviour {
       state = State.WaitingForInput;
       GenerateRandomTile();
       GenerateRandomTile();
-    } else if (state == State.WaitingForInput) {
-      if (Input.GetButtonDown("Left")) {
+    } 
+	else if (state == State.WaitingForInput) {
+		
+		//touch or not ?
+		if (Input.touchCount > 0){
+		Touch touch = Input.touches[0];
+		switch (touch.phase){
+				
+			case TouchPhase.Began:
+				startPos = touch.position;
+			break;
+					
+			case TouchPhase.Ended:
+				float swipeDistVertical = (new Vector3(0, touch.position.y, 0) - new Vector3(0, startPos.y, 0)).magnitude;
+					if (swipeDistVertical > minSwipeDistY){
+						float swipeValue = Mathf.Sign(touch.position.y - startPos.y);
+						//vertical axis movements.
+						if (swipeValue > 0){ touchMove = TouchMovement.Up; }
+						else if (swipeValue < 0){ touchMove = TouchMovement.Down; }
+					}
+					
+				float swipeDistHorizontal = (new Vector3(touch.position.x,0, 0) - new Vector3(startPos.x, 0, 0)).magnitude;
+					if (swipeDistHorizontal > minSwipeDistX){
+						float swipeValue = Mathf.Sign(touch.position.x - startPos.x);
+						if (swipeValue > 0) { touchMove = TouchMovement.Right; }						
+						else if (swipeValue < 0){ touchMove = TouchMovement.Left; }
+					}
+				break;
+			}
+		}
+		else { touchMove = TouchMovement.None; }
+	
+      if (Input.GetButtonDown("Left") || touchMove == TouchMovement.Left) {
         if (MoveTilesLeft()) {
           state = State.CheckingMatches;
         }
-      } else if (Input.GetButtonDown("Right")) {
+      } else if (Input.GetButtonDown("Right") || touchMove == TouchMovement.Right) {
         if (MoveTilesRight()) {
           state = State.CheckingMatches;
         }
-      } else if (Input.GetButtonDown("Up")) {
+      } else if (Input.GetButtonDown("Up") || touchMove == TouchMovement.Up) {
         if (MoveTilesUp()) {
           state = State.CheckingMatches;
         }
-      } else if (Input.GetButtonDown("Down")) {
+      } else if (Input.GetButtonDown("Down") || touchMove == TouchMovement.Down) {
         if (MoveTilesDown()) {
           state = State.CheckingMatches;
         }
@@ -106,6 +155,7 @@ public class GridManager : MonoBehaviour {
         state = State.GameOver;
       }
     }
+	touchMove = TouchMovement.None;
   }
   #endregion
 
